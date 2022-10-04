739e5d143c05212f0ad14de5a85fb979acfabfc9
susmita chougule <Susmita.Chougule@mindtree.com>
2022-10-04 13:11:37 +0530

    first commit

---
+++ src/jenkins/addition.java
@@ -0,0 +1,16 @@
+package jenkins;
+
+public class addition {
+	public static void main(String[]args) {
+		@SuppressWarnings("unused")
+		int n1=40,n2=20,mul,div;
+		
+		mul=n1*n2;
+		div=n1/n2;
+		
+		System.out.println("multiplication os no:"+mul);
+		System.out.println("division os no:"+div);
+		
+	}
+
+}
